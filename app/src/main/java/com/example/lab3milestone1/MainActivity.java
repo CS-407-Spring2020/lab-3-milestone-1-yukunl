bbbb27c1aaf2a6c8c980c93137fabff747a0bc54
akun <aliceliyukun@outlook.com>
2020-02-16 14:01:25 -0600

    - onClick finished

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.lab3milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+               // Log.i("Info",  "Button pressed");
+                openDialog();
+            }
+        });
+
+    }
+
+
+
+    public void openDialog(){
+
     }
 }
